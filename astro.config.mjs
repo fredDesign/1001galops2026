549365806c6849d7c6ae34b5e3621bb08cfa0b8d
fred <fred@emencia.Com>
2026-05-09 20:33:45 +0200

    [CHG] SEO / Schema

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,24 @@
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://1001galops.fr",
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [
+    react(),
+    sitemap({
+      customPages: [
+        "https://1001galops.fr/#balades",
+        "https://1001galops.fr/#evenements",
+        "https://1001galops.fr/#yoga",
+        "https://1001galops.fr/#lieu",
+        "https://1001galops.fr/#apropos",
+      ],
+    }),
+  ],
 })
